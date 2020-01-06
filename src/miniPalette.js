4052d2031510ae2a4f0ee5e71b80a5c3b8ea3f18
Rahul <joshi.rj21@gmail.com>
2020-01-06 13:07:34 +0530

    making colorspalettelist

---
+++ src/miniPalette.js
@@ -0,0 +1,60 @@
+import React, { Component } from 'react';
+import {withStyles} from "@material-ui/styles"
+const styles = {
+    root:{
+        backgroundColor:"white",
+        border:"1px solid black",
+        borderRadius:"5px",
+        padding:"0.5rem",
+        position:"relative",
+        overflow:"hidden",
+        "&:hover":{
+            cursor:"pointer"
+        }
+    },
+    colors:{
+        backgroundColor:"#dae1e4",
+        height:"150px",
+        width:"100%",
+        borderRadius:"5px",
+        overflow:"hidden"
+    },
+    title:{
+        display:"flex",
+        justifyContent:"space-between",
+        alignItems:"center",
+        margin:"0",
+        colors:"black",
+        paddingTop:"0.5rem",
+        fontSize:"1rem",
+        position:"relative"
+    },
+    emoji:{
+        marginLeft:"0.5rem",
+        fontSize:"1.5rem"
+    },
+    miniColor:{
+        height:"25%",
+        width:"20%",
+        display:"inline-block",
+        margin:"0 auto",
+        position:"relative",
+        marginBottom:"-3.5px"
+    }
+};
+
+function MiniPalette(props){
+    const {classes,paletteName,emoji,colors} = props;
+    const miniColorBoxes = colors.map((color)=>(
+    <div className={classes.miniColor} style={{backgroundColor:color.color}} key={color.name}>
+    </div>))
+    return(
+        <div className={props.classes.root}>
+            <div className={props.classes.colors}>
+                {miniColorBoxes}
+            </div>
+            <h5 className="props.classes.title">{props.paletteName} <span className="props.classes.emoji">{props.emoji}</span></h5>
+        </div>
+    )
+}
+export default withStyles(styles)(MiniPalette);
--- src/paletteList.js
+++ src/paletteList.js
@@ -1,16 +1,53 @@
 import React, { Component } from 'react';
+import {Link} from 'react-router-dom';
+import MiniPalette from './miniPalette';
+import {withStyles} from "@material-ui/styles"
 
+const styles ={
+    root:{
+         backgroundColor:"blue",
+         height:"100vh",
+         display:"flex",
+         alignItems:"flex-start",
+         justifyContent:"center"
+    },
+    container:{
+        width:"50%",
+        display:"flex",
+        flexDirection:"column",
+        flexWrap:"wrap",
+        alignItems:"flex-start",
+    },
+    nav:{
+        display:"flex",
+        width:"100%",
+        justifyContent:"space-between",
+        color:"white"
+    },
+    palettes:{
+        boxSizing:"border-box",
+        width:"100%",
+        display:"grid",
+        gridTemplateColumns:"repeat(3,30%)",
+        gap:"5%"
+    }
+}
 class PaletteList extends Component {
     render() {
-        const { palette } = this.props;
-        // console.log(this.props.palette)
+        const { palette ,classes} = this.props;
         return (
-            <div>
-                <h1>React Colors</h1>
-                {palette.map((palettes) => <h1>{palettes.paletteName}</h1>)}
+            <div className={classes.root}>
+                <div className={classes.container}>
+                    <div className={classes.nav}>
+                    <h1>React Colors</h1>
+                    </div>
+                    <div className={classes.palettes}>
+                    {palette.map((palettes) => <MiniPalette {...palettes}/>)}
+                    </div>
+                </div>
             </div>
         )
     }
 }
 
-export default PaletteList;
+export default withStyles(styles)(PaletteList);
